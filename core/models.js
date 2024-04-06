5192c30efec00b0989af4b22a5ae2ba03077dffb
Zerefw <166086021+Zerefw@users.noreply.github.com>
2024-04-06 17:02:06 -0500

    cambios

--- core/models.js
+++ core/models.js
@@ -4,4 +4,3 @@ import { espacioSchema, tiempoSchema, vehiculoSchema } from "./schemas.js";
 export const espacioModel = mongoose.model("Espacio", espacioSchema);
 export const tiempoModel = mongoose.model("Tiempo", tiempoSchema);
 export const vehiculoModel = mongoose.model("Vehiculo", vehiculoSchema);
-
